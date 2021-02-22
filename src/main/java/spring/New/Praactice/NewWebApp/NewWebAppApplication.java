714012f1d290b70eab3dd3a3c27949d1e0565851
J-hub-star <jaibehl33@gmail.com>
2021-02-22 01:01:50 -0600

    first

---
+++ src/main/java/spring/New/Praactice/NewWebApp/NewWebAppApplication.java
@@ -0,0 +1,13 @@
+package spring.New.Praactice.NewWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/New/Praactice/NewWebApp/NewWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.New.Praactice.NewWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
